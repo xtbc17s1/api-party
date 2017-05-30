fee7acf3ccf69fbe797925b116219e99d3ee692c
Akash <akash.raju@outlook.com>
2017-05-30 12:00:12 -0400

    switch user when new user is typed

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './GithubUser.css'
 
 class GithubUser extends Component {
     state = {
@@ -16,10 +17,16 @@ class GithubUser extends Component {
         this.fetchUserData()
     }
 
-    fetchUserData() {
+    fetchUserData(props) {
         fetch(`https://api.github.com/users/${this.props.match.params.username}`)
             .then(response => response.json())
             .then(user => this.setState({ user }))
+    }
+    componentWillReceiveProps(nextProps) {
+        const locationChanged = (nextProps.location !== this.props.location)
+        if (locationChanged) {
+            this.fetchUserData(nextProps)
+        }
     }
     render() {
         const { user } = this.state
