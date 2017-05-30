1920e2307d8d0f6d2de4edf583d0ceebdae16e65
Akash <akash.raju@outlook.com>
2017-05-30 11:48:59 -0400

    get info from github api and render it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 import Github from './Github'
 
--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component {
+    state = {
+        user: {
+            avatar_url: '',
+            login: '',
+            followers: '',
+            following: '',
+            location: '',
+            html_url: '',
+        }
+    }
+    constructor(props) {
+        super(props)
+        this.fetchUserData()
+    }
+
+    fetchUserData() {
+        fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+            .then(response => response.json())
+            .then(user => this.setState({ user }))
+    }
     render() {
-        return <h3>You searched for {this.props.match.params.username}</h3>
+        const { user } = this.state
+        return (
+            <div className="github-user">
+                <img src={user.avatar_url} alt="user"/>
+                <h2>{user.login}</h2>
+                <h3>followers: {user.followers}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
+            </div>
+        )
     }
 }
 
