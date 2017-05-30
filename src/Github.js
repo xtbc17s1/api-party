df7e059e56398c97d450659bd89c94b49a86de78
Akash <akash.raju@outlook.com>
2017-05-30 11:15:58 -0400

    let user type in textbox and add image

--- src/Github.js
+++ src/Github.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react'
 import './Github.css'
 
 class Github extends Component {
+    state = {
+        username: '',
+    }
+    
+    handleChange = (ev) => {
+        const username = ev.currentTarget.value
+        this.setState({ username })
+    }
     render() {
         return (
             <div className="github">
-                <image className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github"/>
+                <div>
+                <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github"/>
+                </div>
                 <form>
                     <div>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
                     </div>
                     <div>
                         <button type="submit">Look up github user</button>
                     </div>
                 </form>
+                <h3>Please enter a username to search on Github</h3>
             </div>
         )
     }
