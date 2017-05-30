e1245b43c4fff803169b62c04dfef72cf4bdb9ec
Seth Baughman <sbaughman1@me.com>
2017-05-30 15:42:56 -0400

    Add styles and onChange handler

--- src/Github.js
+++ src/Github.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react'
+import './Github.css'
 
 class Github extends Component {
+  state = {
+    username: ''
+  }
+
+  handleChange = (ev) => {
+    const username = ev.currentTarget.value
+    this.setState({ username })
+  }
+
   render() {
-    return <h1>No really, github is sweet</h1>
+    return (
+      <div className="github">
+        <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github" />
+        <form>
+          <div>
+            <input 
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange} />
+          </div>
+          <div>
+            <button type="submit">Look up github user</button>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
