cac0a9fec4db69919778eb53f3bc1fc26aa155e0
Akash <akash.raju@outlook.com>
2017-05-30 11:24:31 -0400

    handle form submission and add username to route

--- src/Github.js
+++ src/Github.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import './Github.css'
 
 class Github extends Component {
     state = {
         username: '',
     }
-    
     handleChange = (ev) => {
         const username = ev.currentTarget.value
         this.setState({ username })
+    }
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+
     }
     render() {
         return (
@@ -16,7 +21,7 @@ class Github extends Component {
                 <div>
                 <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github"/>
                 </div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input 
                             type="text" 
@@ -28,7 +33,9 @@ class Github extends Component {
                         <button type="submit">Look up github user</button>
                     </div>
                 </form>
-                <h3>Please enter a username to search on Github</h3>
+
+                <Route exact path="/github" render={() => 
+                    <h3>Please enter a username to search on Github</h3>} />
             </div>
         )
     }
