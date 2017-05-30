30380d7770e5c134642a7ad77303aa211080c17b
Seth Baughman <sbaughman1@me.com>
2017-05-30 16:07:03 -0400

    Fetch Github user data on component load

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component {
+  constructor(props) {
+    super(props)
+    this.fetchUserData()
+  }
+
+  fetchUserData = () => {
+    fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+      .then(response => response.json())
+      .then(user => console.log(user))
+  }
+
   render() {
     return <h1>USER: {this.props.match.params.username}</h1>
   }
