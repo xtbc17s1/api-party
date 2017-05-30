cfda1eaa1e7aa4cc39abe5ab0f9d1fcb67722802
Seth Baughman <sbaughman1@me.com>
2017-05-30 11:24:46 -0400

    Handle form submission and add username to url

--- src/Github.js
+++ src/Github.js
@@ -12,11 +12,16 @@ class Github extends Component {
     this.setState({ username })
   }
 
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+  }
+
   render() {
     return (
       <div className="github">
         <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github"/>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <input type="text"
               value={this.state.username}
